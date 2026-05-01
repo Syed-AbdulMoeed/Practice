b50d280f9b03ded733b8442661d71c62cc161239
Syed-AbdulMoeed <bronzesheild2@gmail.com>
2026-05-01 16:24:50 +0200

    Initial Commit

---
+++ web_dev_practice/Roster/script.js
@@ -0,0 +1,8 @@
+const data = {
+    'Faker': ['Gumayusi', 'Zeus', 'Canna'],
+    'Canna' : ['test', 'test', 'test'],
+    'Zeus' : ['test', 'test', 'test'],
+    'Gumayusi' : ['test', 'test', 'test'],
+    'test' : ['Faker','Faker','Faker']
+}
+
