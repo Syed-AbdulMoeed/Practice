325c9a0d70f16009665901c62cbe0a2ea0f30e98
Syed-AbdulMoeed <bronzesheild2@gmail.com>
2026-06-23 00:13:03 +0200

    added the new components

--- web_dev_practice/Express/vite-project/src/App.jsx
+++ web_dev_practice/Express/vite-project/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './header.jsx'
 import ChatArea from './ChatArea.jsx'
+import InputArea from './InputArea.jsx'
 import './App.css'
 
 function App(){
     return <div className="main-container"> 
         <Header/>
         <ChatArea/>
+        <InputArea/>
         </div>
 }
 
